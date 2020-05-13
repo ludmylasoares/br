99291fc30e9b0709f768bc501c78541516aacfa6
Ludmyla Soares <ludmylaoliveirasoares@gmail.com>
2020-05-12 22:21:22 -0300

    Particles.js Ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ParticlesConfig } from 'particles.js/js/particles.js';
+
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'br';
+export class AppComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+      // https://vincentgarreau.com/particles.js/
+      particlesJS('particles-js', ParticlesConfig, function() {
+          console.log('callback - particles.js config loaded');
+        });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
